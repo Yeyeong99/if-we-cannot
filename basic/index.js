4399ea32d1e4e71f664faa8e0e4074393b71efab
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-08 04:07:20 +0900

    media 추가

--- basic/index.js
+++ basic/index.js
@@ -3,11 +3,6 @@ const moon = document.querySelector("div.moon");
 
 moon.addEventListener("click", function() {
     author.classList.toggle("author");
-    if(author.classList.contains("show")){
-        author.classList.remove("show");
-        author.classList.add("out");
-    }else if(author.classList.contains("out")){
-        author.classList.remove("out");
-        author.classList.add("show");
-    }
+    author.classList.toggle("show");
+
 });
