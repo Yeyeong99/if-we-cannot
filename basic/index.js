bc0050bbe3f5343d213cafe844185a08187afff0
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-08 19:32:45 +0900

    배경 움직임 추가

--- basic/index.js
+++ basic/index.js
@@ -1,8 +1,23 @@
 const author = document.querySelector(".author");
 const moon = document.querySelector("div.moon");
 
+const cloud = document.querySelector(".cloud"),
+cloudOver = document.querySelector(".cloud-over"),
+titleContainer = document.querySelector(".title-container"),
+lineContainer = document.querySelector(".line-container");
+
+const array = [author,moon,cloud,cloudOver,titleContainer,lineContainer,moon]
+
 moon.addEventListener("click", function() {
     author.classList.toggle("author");
     author.classList.toggle("show");
 
+});
+
+window.addEventListener("click", function(){
+
+    cloud.classList.add("show");
+    cloudOver.classList.add("show");
+    titleContainer.classList.add("show");
+    lineContainer.classList.add("show");
 });
