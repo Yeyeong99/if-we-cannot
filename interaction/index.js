9c1885ddd93f289670dd1c5b5286916ddd439c3f
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-10 07:18:02 +0900

    change motion of stars

--- interaction/index.js
+++ interaction/index.js
@@ -6,7 +6,7 @@ function createStars() {
         let size = Math.random() *2;
         let x = Math.floor(Math.random()*window.innerWidth);
         let y = Math.floor(Math.random()*window.innerHeight);
-        
+        star.className="star"
         star.style.left = x + 'px';
         star.style.top = y + 'px';
         star.style.width = 1 + size + 'px';
@@ -16,25 +16,32 @@ function createStars() {
     }
 };
 
-function defultWidth() {
-    const stars = document.querySelectorAll("i");
-    for (i=0; i<=stars.length; i++) {
-        stars[i].style.width = 0 + 'px';
-    };
-};
-
 function changeStarWidth() {
     const stars = document.querySelectorAll("i");
-    for (i=0; i<=stars.length; i++) {
-        if (i%2 === 0) {
-            let x = Math.floor(Math.random()*100);
-            stars[i].style.width = x + 'px';
-            stars[i].classList.toggle("change-angle");
-            stars[i].classList.toggle("change-width");
-        };
+    for (i=0; i<stars.length; i++) {
+        let x = Math.floor(Math.random()*100);
+        stars[i].style.width = x + 'px';
+        stars[i].style.transitionProperty = "width";
+        stars[i].style.transformOrigin = "top left"
+        stars[i].style.transitionDuration ="3s";
+        stars[i].style.transform = "rotate(60deg)";
+        // stars[i].style.transition = "4s";
     };
+    
 };
-
-
+function defaultWidth() {
+    const stars = document.querySelectorAll("i");
+    for (i=0; i<stars.length; i++) {
+        stars[i].style.transitionProperty = "width";
+        stars[i].style.transitionDuration ="10s";
+        let starNum = stars[i].style.width.replace(/[^0-9]/g,'');
+        starNum -=(starNum-1);
+        stars[i].style.width = starNum +"px";
+        console.log(stars[i].style.width)
+    };
+}
 createStars();
-window.addEventListener("click", changeStarWidth);
+window.addEventListener("click",changeStarWidth);
+
+
+// window.addEventListener("click",defaultWidth);/
