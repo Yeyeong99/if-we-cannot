9dfa3e3ed172e6f96b97af1ab479c3f52b0c1717
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-10 08:24:29 +0900

    change shooting star motion

--- interaction/index.js
+++ interaction/index.js
@@ -16,34 +16,22 @@ function createStars() {
     }
 };
 
-function changeStarWidth() {
-    const stars = document.querySelectorAll("i");
-    for (i=0; i<stars.length; i++) {
-        if (i%3 === 0) {
-            let x = Math.floor(Math.random()*100);
-            stars[i].style.width = x + 'px';
-            stars[i].style.transitionProperty = "width";
-            stars[i].style.transformOrigin = "top left"
-            stars[i].style.transitionDuration ="0.5s";
-            stars[i].style.transform = "rotate(-120deg) translate(50px)";
-        };
-        }
-    
-};
-function defaultWidth() {
-    const stars = document.querySelectorAll("i");
-    for (i=0; i<stars.length; i++) {
-        stars[i].style.transitionProperty = "width";
-        stars[i].style.transitionDuration ="1s";
-        stars[i].style.width=1+"px";
-    };
-}
-;
-createStars();
-window.addEventListener("click", defaultWidth);
+
 const stars = document.querySelectorAll("i");
 for (i=0; i<stars.length; i++) {
-    stars[i].addEventListener("mouseover", changeStarWidth);
+    if (i%3 === 0) {
+        let x = Math.floor(Math.random()*100);
+        stars[i].style.width = x + 'px';
+    };
 };
 
-// window.addEventListener("click",defaultWidth);/
+createStars();
+window.addEventListener("click", function() {
+    const stars = document.querySelectorAll("i");
+    for (i=0; i<stars.length; i++) {
+        if (i%19 === 0) {
+            stars[i].classList.toggle("change-location");
+        };
+        
+    }
+});
