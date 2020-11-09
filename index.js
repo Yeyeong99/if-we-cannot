853225c91002dba07aa638a753a6f36de6c6ff34
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-09 17:38:36 +0900

    window resize 이벤트 추가

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const moon = document.querySelector("div.moon");
 const cloud = document.querySelector(".cloud"),
 cloudOver = document.querySelector(".cloud-over"),
 titleContainer = document.querySelector(".title-container"),
+title = document.querySelectorAll(".title"),
 lineContainer = document.querySelector(".line-container"),
 clickBtn = document.querySelector(".click");
 
@@ -18,9 +19,23 @@ moon.addEventListener("click", function() {
 window.addEventListener("click", function(){
     clickBtn.classList.add("hide");
     for (i=0;i<=array.length;i++) {
-        console.log(array[i])
         array[i].classList.add("show");
     }
     clickBtn.classList.remove("click");
 });
 
+function changeTitle () {
+    console.log(title)
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 800) {
+        for(i=0; i<=title.length; i++) {
+            title[i].innerText= "우리가 빛의 속도로\n갈 수 없다면";
+        }
+    } else {
+        for(i=0; i<=title.length; i++) {
+            title[i].innerText= "우리가 빛의 속도로 갈 수 없다면";
+        }
+    }
+}
+
+window.addEventListener("resize", changeTitle);
