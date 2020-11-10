03252f269778885e565949edb44a0e76c4dde80b
Yeyeong99 <Isabel025484@gmail.com>
2020-11-10 18:47:09 +0900

    add buttons

--- interaction/index.js
+++ interaction/index.js
@@ -1,8 +1,10 @@
 const earth = document.querySelector(".earth"),
  earthContainer = document.querySelector(".earth-container"),
  author = document.querySelector(".author-container"),
- scene = document.querySelector(".scene");
-function createStars() {
+ scene = document.querySelector(".scene"),
+ authorBtn = document.querySelector(".author-btn");
+ 
+ function createStars() {
     const scene = document.querySelector(".scene");
     for (i=0; i<=500; i++) {
         const star = document.createElement("i");
@@ -37,8 +39,12 @@ function shootingStars() {
 window.addEventListener("click", shootingStars);
 
 earth.addEventListener("mouseover", function() {
-    earth.style.transform = "scale(1.5)";
-    author.style.bottom = "20%";
+    earth.style.transform = "scale(1.2)";
+    author.style.bottom = "17%";
+    const buttons = document.querySelectorAll("button");
+    for (i=0; i<buttons.length; i++) {
+        buttons[i].classList.remove("hide");
+    };
 });
 
 earth.addEventListener("mouseleave", function() {
@@ -46,6 +52,6 @@ earth.addEventListener("mouseleave", function() {
     earth.style.transform="";
 });
 
-earth.addEventListener("click", function() {
+authorBtn.addEventListener("click", function() {
     author.classList.toggle("show");
-})
+});
